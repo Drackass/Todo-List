3bd80bf5b79f088184191fa354d6239524903a0d
SAUZET-Leny <leny.sauzet@gmail.com>
2023-07-06 16:32:27 +0200

    add todo complet, begin delete todo

--- main.js
+++ main.js
@@ -1,23 +1,48 @@
-let todos = ['Get groceries', 'Wash car', 'Make dinner'];
+const todos = [{
+    title: 'Get groceries',
+    dueDate: '2021-10-04',
+}, {
+    title: 'Wash car',
+    dueDate: '2021-02-03',
+
+}, {
+    title: 'Make dinner',
+    dueDate: '2021-03-04',
+}];
 
 render();
 
 function addTodo() {
-    let textbox = document.getElementById('todo-title');
-    let title = textbox.value;
-    todos.push(title);
+    const textbox = document.getElementById('todo-title');
+    const title = textbox.value;
+
+    const datePicker = document.getElementById('date-picker');
+    const dueDate = datePicker.value;
+
+    todos.push({
+        title: title,
+        dueDate: dueDate
+    });
 
     render();
+}
+
+function deleteTodo() {
 }
 
 function render() {
     document.getElementById('todo-list').innerHTML = '';
 
-    todos.forEach(function (todoTitle){
-        let element = document.createElement('div');
-        element.innerText = todoTitle;
+    todos.forEach(function (todo){
+        const element = document.createElement('div');
+        element.innerText = todo.title + ' ' + todo.dueDate;
+        const deleteButton = document.createElement('button');
+        deleteButton.innerText = 'Delete';
+        deleteButton.style = 'margin-left: 12px;';
+        deleteButton.onclick = deleteTodo;
 
-        let todolist = document.getElementById('todo-list');
+        element.appendChild(deleteButton);
+        const todolist = document.getElementById('todo-list');
         todolist.appendChild(element);
     });
 }
