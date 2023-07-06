55f77bc035072f0678bf9473d53e97d189bef9e3
SAUZET-Leny <leny.sauzet@gmail.com>
2023-07-06 14:21:07 +0200

    first function

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ let todo1 = 'Get groceries';
 let todo2 = 'wash car';
 let todo3 = 'Make dinner';
 
-let element = document.createElement('div');
-element.innerText = todo1;
-document.body.appendChild(element);
+function addTodo(todoTitle) {
+    let element = document.createElement('div');
+    element.innerText = todoTitle;
+    document.body.appendChild(element);
+}
+
+addTodo(todo1);
+addTodo(todo2);
+addTodo(todo3);
