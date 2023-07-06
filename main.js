382027e364802d6edb3d29289fa018c68e618c2a
SAUZET-Leny <leny.sauzet@gmail.com>
2023-07-06 14:31:08 +0200

    use foreach

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
-let todo1 = 'Get groceries';
-let todo2 = 'wash car';
-let todo3 = 'Make dinner';
+let todos = ['Get groceries', 'Wash car', 'Make dinner'];
+todos.push('another todo')
 
-function addTodo(todoTitle) {
+todos.forEach(function (todoTitle){
     let element = document.createElement('div');
     element.innerText = todoTitle;
     document.body.appendChild(element);
-}
-
-addTodo(todo1);
-addTodo(todo2);
-addTodo(todo3);
+});
