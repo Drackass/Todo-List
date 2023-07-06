84343e61c91b11e645f011c3f3a4a544d89c19e4
SAUZET-Leny <leny.sauzet@gmail.com>
2023-07-06 14:41:08 +0200

    add todo function

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 let todos = ['Get groceries', 'Wash car', 'Make dinner'];
 todos.push('another todo')
 
-todos.forEach(function (todoTitle){
-    let element = document.createElement('div');
-    element.innerText = todoTitle;
-    document.body.appendChild(element);
-});
+render();
+
+function addTodo() {
+    let textbox = document.getElementById('todo-title');
+    let title = textbox.value;
+    todos.push(title);
+
+    render();
+}
+
+function render() {
+    todos.forEach(function (todoTitle){
+        let element = document.createElement('div');
+        element.innerText = todoTitle;
+        document.body.appendChild(element);
+    });
+}
