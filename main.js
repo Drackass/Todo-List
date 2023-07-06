34d449502f3b88130552de06041df625cbeb5b8e
SAUZET-Leny <leny.sauzet@gmail.com>
2023-07-06 14:12:21 +0200

    first render of todo  exemples with javascript and HTML

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-alert('hello');
+let todo1 = 'Get groceries';
+let todo2 = 'wash car';
+let todo3 = 'Make dinner';
+
+let element = document.createElement('div');
+element.innerText = todo1;
+document.body.appendChild(element);
